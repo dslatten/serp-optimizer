6a3cda4cd25bea75d73a8081dd78bc4d8b892cb6
Darren Slatten <darrenslatten@gmail.com>
2012-06-19 17:26:46 -0500

    Added custom character limits

---
+++ tools/get-serp-styles.js
@@ -0,0 +1,10 @@
+/* Bookmarklet:
+
+javascript:(function(){
+var getSerpStyle=document.createElement('script');
+document.getElementsByTagName('head')[0].appendChild(getSerpStyle).src='http://www.darrenslatten.com/serp-optimizer/tools/get-serp-styles.js';
+document.getElementsByTagName('head')[0].removeChild(getSerpStyle);
+})();
+
+*/
+
