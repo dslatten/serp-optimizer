093c3f6c98498cf24b39114df83a63398b6ec6dc
Darren Slatten <darrenslatten@gmail.com>
2012-06-20 00:55:55 -0500

    Unminify CSS

--- tools/get-serp-styles.js
+++ tools/get-serp-styles.js
@@ -21,23 +21,16 @@ document.getElementsByTagName('head')[0].removeChild(getSerpStyle);
     d.close();
     b = d.body;
 
-    // This function's purpose is to prepare the CSS output in such a way that 
-    // I can copy+paste it into /css/google.less, save it, and my LESS compiler 
-    // (http://wearekiss.com/simpless) won't throw any errors.
     function preprocessCSS(s) {
         s = s.replace(/\s*}\s*/g, '}');
         s = s.replace(/\s*\{\s*/g, '{');
+        s = s.replace(/\s*,\s*/g, ',');
         s = s.replace(/\s*;\s*/g, ';');
         s = s.replace(/\s*:\s*/g, ':');
-        s = s.replace(/(^\s+|\s+$)/gm, '');
         s = s.replace(/}/g, '}\n');
         s = s.replace(/\{/g, '{\n');
-        s = s.replace(/([^;\{\s])}/g, '$1;}');
         s = s.replace(/;/g, ';\n');
-        s = s.replace(/:(.+)\\0\/(;|})/g, ':~"$1\\0/";'); // http://lesscss.org/#-escaping
-        s = s.replace(/:\+(.+);/g, ':~"+$1";');
-        s = s.replace(/^0;$/gm, ''); // For whatever reason (typo?), Google had a CSS property of "0". This kills it to prevent LESS compiler error.
-        s = s.replace(/\)background-color:/g, ');\nbackground-color:'); // Another typo (?)
+        s = s.replace(/(^\s+|\s+$)/gm, '');
         return s;
     }
 
