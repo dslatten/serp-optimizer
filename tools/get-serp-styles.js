504b75a10e9e0637c8c36bb0c8bc1fc66ba42f04
Darren Slatten <darrenslatten@gmail.com>
2012-06-19 23:34:31 -0500

    Stupid regular expressions!

--- tools/get-serp-styles.js
+++ tools/get-serp-styles.js
@@ -34,7 +34,7 @@ document.getElementsByTagName('head')[0].removeChild(getSerpStyle);
         s = s.replace(/([^;])}/g, '$1;}');
         s = s.replace(/;/g, ';\n');
         s = s.replace(/:(.+)\\0\/(;|})/g, ':~"$1\\0/";'); // http://lesscss.org/#-escaping
-        s = s.replace(/0;\n/g, '');
+        s = s.replace(/^0;$/g, '');
         return s;
     }
 
