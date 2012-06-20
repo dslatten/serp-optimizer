fb6d71804a50c0dbe66b230be17011603b375417
Darren Slatten <darrenslatten@gmail.com>
2012-06-19 19:43:09 -0500

    Updated SERP style getter

--- tools/get-serp-styles.js
+++ tools/get-serp-styles.js
@@ -1,4 +1,9 @@
-/* Bookmarklet:
+/*
+
+This code is based on bookmarklet found here:
+https://www.squarefree.com/bookmarklets/webdevel.html#view_style_sheets
+
+Bookmarklet to run hosted code:
 
 javascript:(function(){
 var getSerpStyle=document.createElement('script');
@@ -17,29 +22,37 @@ document.getElementsByTagName('head')[0].removeChild(getSerpStyle);
     d.open();
     d.close();
     b = d.body;
+
     function trim(s) {
         return s.replace(/^\s*\n/, '').replace(/\s*$/, '');
     }
+
     function iff(a,b,c) {
         return b ? a+b+c : '';
     }
+
     function add(h) {
         b.appendChild(h);
     }
+
     function makeTag(t) {
         return d.createElement(t);
     }
+
     function makeText(tag,text) {
         t = makeTag(tag);
         t.appendChild(d.createTextNode(text));
         return t;
     }
+
     add(makeText('style', 'iframe{width:100%; height:18em; border:1px solid;'));
-    add(makeText('h3', d.title='Style sheets in ' + location.href));
+//  add(makeText('h3', d.title='Style sheets in ' + location.href));
+
     for (i = 0; i < s.length; ++i) {
-        add(makeText('h4','Inline style sheet'  + iff(' title="',s[i].title,'"')));
+//      add(makeText('h4', 'Inline style sheet'  + iff(' title="', s[i].title, '"')));
         add(makeText('pre', trim(s[i].innerHTML)));
     }
+
     for (i = 0; i < ex.length; ++i) {
         rs = ex[i].rel.split(' ');
         for (j = 0; j < rs.length; ++j) {
