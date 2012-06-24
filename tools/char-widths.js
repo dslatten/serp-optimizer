3234c2dfdfe09e9ccaa0cdb5dab0e2768b449c7b
Darren Slatten <darrenslatten@gmail.com>
2012-06-24 03:17:37 -0500

    Updated tools

--- tools/char-widths.js
+++ tools/char-widths.js
@@ -1,3 +1,15 @@
+/*
+
+Bookmarklet to run hosted code:
+
+javascript:(function(){
+var getSerpStyle=document.createElement('script');
+document.getElementsByTagName('head')[0].appendChild(getSerpStyle).src='http://serpimp.com/tools/char-widths.js';
+document.getElementsByTagName('head')[0].removeChild(getSerpStyle);
+})();
+
+*/
+
 (function() {
 
     if (!window.jQuery) {
--- tools/get-serp-styles.js
+++ tools/get-serp-styles.js
@@ -7,7 +7,7 @@ Bookmarklet to run hosted code:
 
 javascript:(function(){
 var getSerpStyle=document.createElement('script');
-document.getElementsByTagName('head')[0].appendChild(getSerpStyle).src='http://www.darrenslatten.com/serp-optimizer/tools/get-serp-styles.js';
+document.getElementsByTagName('head')[0].appendChild(getSerpStyle).src='http://serpimp.com/tools/get-serp-styles.js';
 document.getElementsByTagName('head')[0].removeChild(getSerpStyle);
 })();
 
