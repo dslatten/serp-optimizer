5af7ee3c9d50335f6f2b101f71e20106e8497828
Darren Slatten <darrenslatten@gmail.com>
2012-06-27 11:00:03 -0500

    Updated char-widths.js

--- tools/char-widths.js
+++ tools/char-widths.js
@@ -3,22 +3,40 @@
 Bookmarklet to run hosted code:
 
 javascript:(function(){
-var getSerpStyle=document.createElement('script');
-document.getElementsByTagName('head')[0].appendChild(getSerpStyle).src='http://serpimp.com/tools/char-widths.js';
-document.getElementsByTagName('head')[0].removeChild(getSerpStyle);
+var charWidths=document.createElement('script');
+document.getElementsByTagName('head')[0].appendChild(charWidths).src='http://serpimp.com/tools/char-widths.js';
+document.getElementsByTagName('head')[0].removeChild(charWidths);
 })();
 
 */
 
-(function() {
+// Poll instead of a setTimeout hack
+// http://paulirish.com/2008/best-practice-poll-instead-of-a-settimeout-hack/
 
+function executeWhenLoaded(func) {  
+    for (var i = 1; i < arguments.length; i++) {
+        if (! window[ arguments[i] ]) {
+            setTimeout(arguments.callee, 50);      
+            return;
+        } 
+    }
+
+    func();
+}
+
+
+(function() {
     if (!window.jQuery) {
        var script = document.createElement('script');
-       script.id = 'mofo_script';
        script.type = 'text/javascript';
        script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js';
        document.getElementsByTagName('head')[0].appendChild(script);
+       document.getElementsByTagName('head')[0].removeChild(script);
     }
-
 })();
 
+
+
+executeWhenLoaded(function() { 
+    alert('jQuery = ' + window.jQuery);
+},'jQuery');
