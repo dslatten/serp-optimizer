e47e393cbbf91b3818a24746642a1bfbf68e6bce
Darren Slatten <darrenslatten@gmail.com>
2013-03-31 14:13:14 -0500

    Testing an example

--- app/main.js
+++ app/main.js
@@ -9,13 +9,13 @@ define(['durandal/app', 'durandal/system', 'durandal/viewLocator'],
     
     system.debug(true);
     
-    app.title = 'Durandal Samples';
+    app.title = 'SERP Imp Application';
     app.start().then(function () {
         //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
         //Look for partial views in a 'views' folder in the root.
         viewLocator.useConvention();
 
         app.adaptToDevice();
-        app.setRoot('samples/shell');
+        app.setRoot('views/shell');
     });
 });
